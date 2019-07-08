718ec1ac4835c4475e7623c9f2500f8da73e007d
Leonardo Alves <leonardo@squad.com.br>
2019-07-08 08:22:30 -0300

    Implements retrofit cache for offline access

--- app/src/main/java/com/leonardoalves/githubrepository/modules/RepositoryModule.kt
+++ app/src/main/java/com/leonardoalves/githubrepository/modules/RepositoryModule.kt
@@ -1,23 +1,55 @@
 package com.leonardoalves.githubrepository.modules
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import com.google.gson.GsonBuilder
 import com.leonardoalves.githubrepository.repository.GitHubApi
 import com.leonardoalves.githubrepository.repository.repositories.GithubRepository
 import io.reactivex.schedulers.Schedulers
+import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 const val SERVER_URL = "https://api.github.com/"
-const val TIMEOUT = 120L
 
-private fun createOkHttpClient(): OkHttpClient {
+private fun createOkHttpClient(context: Context): OkHttpClient {
+    val cacheSize = (5 * 1024 * 1024).toLong()
+    val cache = Cache(context.cacheDir, cacheSize)
+
+    val cacheControlInterceptor = Interceptor { chain ->
+        val originalResponse = chain.proceed(chain.request())
+        val cacheControl = originalResponse.header("Cache-Control")
+        if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
+            cacheControl.contains("must-revalidate") || cacheControl.contains("max-age=0")
+        ) {
+            originalResponse.newBuilder()
+                .removeHeader("Pragma")
+                .header("Cache-Control", "public, max-age=" + 5000)
+                .build()
+        } else {
+            originalResponse
+        }
+    }
+
+    val offlineCacheInterceptor = Interceptor { chain ->
+        var request = chain.request()
+        if (!hasNetwork(context)!!) {
+            request = request.newBuilder()
+                .removeHeader("Pragma")
+                .header("Cache-Control", "public, only-if-cached")
+                .build()
+        }
+        chain.proceed(request)
+    }
     return OkHttpClient.Builder()
-        .readTimeout(TIMEOUT, TimeUnit.SECONDS)
-        .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+        .cache(cache)
+        .addNetworkInterceptor(cacheControlInterceptor)
+        .addInterceptor(offlineCacheInterceptor)
         .build()
 }
 
@@ -31,10 +63,19 @@ inline fun <reified T> createWebService(okHttpClient: OkHttpClient, url: String)
 }
 
 val remoteDataSourceModule = module {
-    single { createOkHttpClient() }
+    single { createOkHttpClient(get()) }
     single { createWebService<GitHubApi>(get(), SERVER_URL) }
 }
 
 val repositoryModule = module {
     single { GithubRepository(get()) }
+}
+
+private fun hasNetwork(context: Context): Boolean? {
+    var isConnected: Boolean? = false // Initial Value
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+    if (activeNetwork != null && activeNetwork.isConnected)
+        isConnected = true
+    return isConnected
 }
